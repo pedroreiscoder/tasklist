f5334a60206bc8f7d3d91c88a9adf6ce45b38a95
pedroreis <pedroreiscoder@gmail.com>
2020-09-15 23:46:41 -0300

    Logs implementados na controller de tags.

--- TaskListApi/TaskListApi/Controllers/TagsController.cs
+++ TaskListApi/TaskListApi/Controllers/TagsController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TaskListApi.Data;
 using TaskListApi.Dtos;
 using TaskListApi.Models;
@@ -18,11 +19,13 @@ namespace TaskListApi.Controllers
     {
         private readonly ITagsRepo _repository;
         private readonly IMapper _mapper;
+        private readonly ILogger<TagsController> _logger;
 
-        public TagsController(ITagsRepo repository, IMapper mapper)
+        public TagsController(ITagsRepo repository, IMapper mapper, ILogger<TagsController> logger)
         {
             _repository = repository;
             _mapper = mapper;
+            _logger = logger;
         }
 
         /// <summary>
@@ -32,6 +35,7 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<TagReadDto>> GetTags()
         {
+            _logger.LogInformation("Consultando as tags cadastradas");
             IEnumerable<Tag> tags = _repository.GetTags();
 
             return Ok(_mapper.Map<IEnumerable<TagReadDto>>(tags));
@@ -48,11 +52,16 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult PutTag(long id, TagUpdateDto tagUpdateDto)
         {
+            _logger.LogInformation("Consultando a tag de Id: {id}", id);
             Tag tag = _repository.GetTagById(id);
 
             if (tag == null)
+            {
+                _logger.LogWarning("A tag de Id: {id} não existe", id);
                 return NotFound();
+            }
 
+            _logger.LogInformation("Editando a tag de Id: {id}", id);
             _mapper.Map(tagUpdateDto, tag);
             _repository.SaveChanges();
 
@@ -70,17 +79,24 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult PatchTag(long id, JsonPatchDocument<TagUpdateDto> jsonPatchDocument)
         {
+            _logger.LogInformation("Consultando a tag de Id: {id}", id);
             Tag tag = _repository.GetTagById(id);
 
             if (tag == null)
+            {
+                _logger.LogWarning("A tag de Id: {id} não existe", id);
                 return NotFound();
+            }
 
+            _logger.LogInformation("Aplicando a alteração na classe de DTO");
             TagUpdateDto tagUpdateDto = _mapper.Map<TagUpdateDto>(tag);
             jsonPatchDocument.ApplyTo(tagUpdateDto, ModelState);
 
+            _logger.LogInformation("Validando a alteração na classe de DTO");
             if (!TryValidateModel(tagUpdateDto))
                 return ValidationProblem(ModelState);
 
+            _logger.LogInformation("Alterando a tag de Id: {id}", id);
             _mapper.Map(tagUpdateDto, tag);
             _repository.SaveChanges();
 
@@ -96,11 +112,16 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult DeleteTag(long id)
         {
+            _logger.LogInformation("Consultando a tag de Id: {id}", id);
             Tag tag = _repository.GetTagById(id);
 
             if (tag == null)
+            {
+                _logger.LogWarning("A tag de Id: {id} não existe", id);
                 return NotFound();
+            }
 
+            _logger.LogInformation("Removendo a tag de Id: {id}", id);
             _repository.DeleteTag(tag);
             _repository.SaveChanges();
 
