8f84843d411ff2230ed4e38a610c10aa5d58e961
pedroreis <pedroreiscoder@gmail.com>
2020-09-16 14:54:51 -0300

    Logs implementados na controller de tasklists.

--- TaskListApi/TaskListApi/Controllers/TaskListsController.cs
+++ TaskListApi/TaskListApi/Controllers/TaskListsController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TaskListApi.Data;
 using TaskListApi.Dtos;
 using TaskListApi.Models;
@@ -21,6 +22,7 @@ namespace TaskListApi.Controllers
         private readonly ITagsRepo _tagsRepo;
         private readonly ITaskTagsRepo _taskTagsRepo;
         private readonly IMapper _mapper;
+        private readonly ILogger<TaskListsController> _logger;
 
         public TaskListsController
         (
@@ -28,7 +30,8 @@ namespace TaskListApi.Controllers
             ITasksRepo tasksRepo, 
             ITagsRepo tagsRepo, 
             ITaskTagsRepo taskTagsRepo, 
-            IMapper mapper
+            IMapper mapper,
+            ILogger<TaskListsController> logger
         )
         {
             _taskListsRepo = taskListsRepo;
@@ -36,6 +39,7 @@ namespace TaskListApi.Controllers
             _tagsRepo = tagsRepo;
             _taskTagsRepo = taskTagsRepo;
             _mapper = mapper;
+            _logger = logger;
         }
 
         /// <summary>
@@ -45,6 +49,7 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<TaskListReadDto>> GetTaskLists()
         {
+            _logger.LogInformation("Consultando as listas de tarefas cadastradas");
             IEnumerable<TaskList> taskLists = _taskListsRepo.GetTaskLists();
 
             List<Tag> tags;
@@ -53,6 +58,7 @@ namespace TaskListApi.Controllers
 
             foreach (TaskList taskList in taskLists)
             {
+                _logger.LogInformation("Mapeando a lista de tarefas de Id: {id} para a classe de DTO", taskList.Id);
                 taskListReadDto = _mapper.Map<TaskListReadDto>(taskList);
 
                 if (taskList.Tasks != null && taskList.Tasks.Count > 0)
@@ -61,12 +67,14 @@ namespace TaskListApi.Controllers
                     {
                         if (taskList.Tasks[i].TaskTags != null && taskList.Tasks[i].TaskTags.Count > 0)
                         {
+                            _logger.LogInformation("Mapeando as tags da tarefa de Id: {id} para a classe de DTO", taskList.Tasks[i].Id);
                             tags = taskList.Tasks[i].TaskTags.Select(tt => tt.Tag).ToList();
                             taskListReadDto.Tasks[i].Tags = _mapper.Map<List<TagReadDto>>(tags);
                         }
                     }
                 }
 
+                _logger.LogInformation("Adicionando a lista de tarefas de Id: {id} na lista de retorno", taskList.Id);
                 taskListReadDtos.Add(taskListReadDto);
             }
 
@@ -82,11 +90,16 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<TaskListReadDto> GetTaskListById(long id)
         {
+            _logger.LogInformation("Consultando a lista de tarefas de Id: {id}", id);
             TaskList taskList = _taskListsRepo.GetTaskListById(id);
 
             if (taskList == null)
+            {
+                _logger.LogWarning("A lista de tarefas de Id: {id} não existe", id);
                 return NotFound();
+            }
 
+            _logger.LogInformation("Mapeando a lista de tarefas de Id: {id} para a classe de DTO", id);
             TaskListReadDto taskListReadDto = _mapper.Map<TaskListReadDto>(taskList);
 
             if (taskList.Tasks != null && taskList.Tasks.Count > 0)
@@ -97,6 +110,7 @@ namespace TaskListApi.Controllers
                 {
                     if (taskList.Tasks[i].TaskTags != null && taskList.Tasks[i].TaskTags.Count > 0)
                     {
+                        _logger.LogInformation("Mapeando as tags da tarefa de Id: {id} para a classe de DTO", taskList.Tasks[i].Id);
                         tags = taskList.Tasks[i].TaskTags.Select(tt => tt.Tag).ToList();
                         taskListReadDto.Tasks[i].Tags = _mapper.Map<List<TagReadDto>>(tags);
                     }
@@ -115,8 +129,10 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<TaskListCreateDto> PostTaskList(TaskListCreateDto taskListCreateDto)
         {
+            _logger.LogInformation("Mapeando a lista de tarefas a ser cadastrada para a model");
             TaskList taskList = _mapper.Map<TaskList>(taskListCreateDto);
 
+            _logger.LogInformation("Cadastrando a lista de tarefas no banco de dados");
             _taskListsRepo.PostTaskList(taskList);
             _taskListsRepo.SaveChanges();
 
@@ -134,6 +150,7 @@ namespace TaskListApi.Controllers
                             TaskId = taskList.Tasks[i].Id
                         };
 
+                        _logger.LogInformation("Cadastrando as tags da tarefa de Id: {id} no banco de dados", taskList.Tasks[i].Id);
                         foreach (TagCreateDto tagCreateDto in taskListCreateDto.Tasks[i].Tags)
                         {
                             tag = _mapper.Map<Tag>(tagCreateDto);
@@ -148,6 +165,7 @@ namespace TaskListApi.Controllers
                 }
             }
 
+            _logger.LogInformation("Montando objeto de retorno");
             TaskList taskListCreated = _taskListsRepo.GetTaskListById(taskList.Id);
             TaskListReadDto taskListReadDto = _mapper.Map<TaskListReadDto>(taskListCreated);
 
@@ -179,11 +197,16 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult PutTaskList(long id, TaskListUpdateDto taskListUpdateDto)
         {
+            _logger.LogInformation("Consultando a lista de tarefas de Id: {id}", id);
             TaskList taskList = _taskListsRepo.GetTaskListById(id);
 
             if (taskList == null)
+            {
+                _logger.LogWarning("A lista de tarefas de Id: {id} não existe", id);
                 return NotFound();
+            }
 
+            _logger.LogInformation("Editando a lista de tarefas de Id: {id}", id);
             _mapper.Map(taskListUpdateDto, taskList);
             _taskListsRepo.SaveChanges();
 
@@ -201,17 +224,24 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult PatchTaskList(long id, JsonPatchDocument<TaskListUpdateDto> jsonPatchDocument)
         {
+            _logger.LogInformation("Consultando a lista de tarefas de Id: {id}", id);
             TaskList taskList = _taskListsRepo.GetTaskListById(id);
 
             if (taskList == null)
+            {
+                _logger.LogWarning("A lista de tarefas de Id: {id} não existe", id);
                 return NotFound();
+            }
 
+            _logger.LogInformation("Aplicando a alteração na classe de DTO");
             TaskListUpdateDto taskListUpdateDto = _mapper.Map<TaskListUpdateDto>(taskList);
             jsonPatchDocument.ApplyTo(taskListUpdateDto, ModelState);
 
+            _logger.LogInformation("Validando a alteração feita na classe de DTO");
             if (!TryValidateModel(taskListUpdateDto))
                 return ValidationProblem(ModelState);
 
+            _logger.LogInformation("Alterando a lista de tarefas de Id: {id}", id);
             _mapper.Map(taskListUpdateDto, taskList);
             _taskListsRepo.SaveChanges();
 
@@ -227,11 +257,16 @@ namespace TaskListApi.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult DeleteTaskList(long id)
         {
+            _logger.LogInformation("Consultando a lista de tarefas de Id: {id}", id);
             TaskList taskList = _taskListsRepo.GetTaskListById(id);
 
             if (taskList == null)
+            {
+                _logger.LogWarning("A lista de tarefas de Id: {id} não existe", id);
                 return NotFound();
+            }
 
+            _logger.LogInformation("Removendo a lista de tarefas de Id: {id}", id);
             _taskListsRepo.DeleteTaskList(taskList);
             _taskListsRepo.SaveChanges();
 
